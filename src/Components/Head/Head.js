8e698eea504afb002cf537d00fec5a8215b8aa14
Hermes <hermeschigoziri@gmail.com>
2022-01-01 12:40:02 +0100

    Routed the Modal describing me to the UI component

--- src/Components/Head/Head.js
+++ src/Components/Head/Head.js
@@ -1,37 +1,32 @@
-import { useState } from 'react';
 import Card from '../Card/Card';
-import Modal from '../Modal/Modal';
 import styles from './Head.module.css';
 
-const AboutMe = {
-    title: "About Me",
-    message: `I'm Hermes, an entry-level developer. I work with C++ and Qt to build GUI applications, I also work with web technologies
-    to build simple websites. I'm working towards mobile development with React-Native but at the moment working on my web-dev skills.`
-}
-export default function Head()
+export default function Head(props)
 {
-    const [clicked, setClicked] = useState(false);
-
-    const clickHandler = () => {
-        setClicked((current) => {
-            return !current;
-        });
-    }
     return (
-        <>
-            { clicked && <Modal title={AboutMe.title} message={AboutMe.message} exitModal={clickHandler}/> }
-            <Card className={`${clicked === false ? styles["head-anim"] : styles["head-no-anim"]}`}>
+      <Card
+        className={`${
+          props.clicked === false ? styles["head-anim"] : styles["head-no-anim"]
+        }`}
+      >
+        <div className={styles["threads"]}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
 
-                <div className={styles["threads"]}><span></span><span></span><span></span><span></span></div>
-                
-                <Card className={styles["title"]} onClicked={clickHandler}>
-                    <span className={styles["title-name"]}>Hermes</span>
-                    <span className={styles["title-role"]}>Junior Dev</span>
-                </Card>
-                
-                <div className={styles["threads"]}><span></span><span></span><span></span><span></span></div>
+        <Card className={styles["title"]} onClicked={props.clickHandler}>
+          <span className={styles["title-name"]}>Hermes</span>
+          <span className={styles["title-role"]}>Junior Dev</span>
+        </Card>
 
-            </Card>
-        </>
-    )
+        <div className={styles["threads"]}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </Card>
+    );
 }
--- src/Components/UI.js
+++ src/Components/UI.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from './Head/Head';
 import Main from './Main/Main';
 import Footer from './Footer/Footer';
+import Modal from './Modal/Modal';
 
 import styles from './ui.module.css';
 
+const AboutMe = {
+    title: "About Me",
+    message: <p>I'm Hermes, an entry-level developer. I work with C++ and Qt to build GUI applications, I also work with web technologies
+    to build simple websites. I'm working towards mobile development with React-Native but at the moment working on my web-dev skills.</p>
+}
+
 const UI = () => {
+    const [clicked, setClicked] = useState(false);
+
+    const clickHandler = () => {
+        setClicked((current) => {
+            return !current;
+        });
+    }
     return (
-        <div className={styles["ui"]}>
-            <Head />
-            <Main />
-            <Footer />
-        </div>
+        <>
+            { clicked && <Modal title={AboutMe.title} message={AboutMe.message} exitModal={clickHandler}/> }
+            <div className={styles["ui"]}>
+                <Head clicked={clicked} clickHandler={clickHandler}/>
+                <Main />
+                <Footer />
+            </div>
+        </>
     );
 };
 
