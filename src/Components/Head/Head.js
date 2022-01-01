70ebb7bedae09175428465cce80202b452eebb96
Hermes <hermeschigoziri@gmail.com>
2022-01-01 12:22:16 +0100

    Add a modal containing info about me.

--- src/Components/Head/Head.js
+++ src/Components/Head/Head.js
@@ -1,28 +1,37 @@
 import { useState } from 'react';
 import Card from '../Card/Card';
+import Modal from '../Modal/Modal';
 import styles from './Head.module.css';
 
+const AboutMe = {
+    title: "About Me",
+    message: `I'm Hermes, an entry-level developer. I work with C++ and Qt to build GUI applications, I also work with web technologies
+    to build simple websites. I'm working towards mobile development with React-Native but at the moment working on my web-dev skills.`
+}
 export default function Head()
 {
     const [clicked, setClicked] = useState(false);
+
     const clickHandler = () => {
         setClicked((current) => {
-            console.log(clicked);
             return !current;
         });
     }
     return (
-        <Card className={`${clicked === false ? styles["head-anim"] : styles["head-no-anim"]}`}>
+        <>
+            { clicked && <Modal title={AboutMe.title} message={AboutMe.message} exitModal={clickHandler}/> }
+            <Card className={`${clicked === false ? styles["head-anim"] : styles["head-no-anim"]}`}>
 
-            <div className={styles["threads"]}><span></span><span></span><span></span><span></span></div>
-            
-            <Card className={styles["title"]} onClicked={clickHandler}>
-                <span className={styles["title-name"]}>Hermes</span>
-                <span className={styles["title-role"]}>Junior Dev</span>
+                <div className={styles["threads"]}><span></span><span></span><span></span><span></span></div>
+                
+                <Card className={styles["title"]} onClicked={clickHandler}>
+                    <span className={styles["title-name"]}>Hermes</span>
+                    <span className={styles["title-role"]}>Junior Dev</span>
+                </Card>
+                
+                <div className={styles["threads"]}><span></span><span></span><span></span><span></span></div>
+
             </Card>
-            
-            <div className={styles["threads"]}><span></span><span></span><span></span><span></span></div>
-
-        </Card>
+        </>
     )
 }
