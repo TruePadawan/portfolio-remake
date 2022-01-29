87bdef16024850e5de85a525ac77e993829bb3af
Hermes <hermeschigoziri@gmail.com>
2022-01-29 18:14:25 +0100

    Add a TechStackItem component

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,30 +1,57 @@
-import React from 'react';
-import Card from '../Card/Card';
-import Contact from './Contact/Contact';
-import Projects from './Projects/Projects';
+import React from "react";
+import ReactTooltip from "react-tooltip";
+import Card from "../Card/Card";
+import Contact from "./Contact/Contact";
+import Projects from "./Projects/Projects";
 
-import styles from './Main.module.css';
+import styles from "./Main.module.css";
 
-export default function Main()
-{
-    return (
-        <Card className={styles["container"]}>
-            <div className={styles["tech-stack"]}>
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-line.svg" alt="cPlusPlus svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/qt/qt-original.svg" alt="Qt svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="React svg" />
-            </div>
+const TechStackItem = (props) => {
+  return (
+    <div>
+      <img src={props.src} alt={props.alt} data-tip data-for={props.alt} />
+      <ReactTooltip id={props.alt} type="light" />
+    </div>
+  );
+};
 
-            <div className={styles["contact-and-project"]}>
-                <Contact />
-                <Projects />
-            </div>
+export default function Main() {
+  return (
+    <Card className={styles["container"]}>
+      <div className={styles["tech-stack"]}>
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-line.svg"
+          alt="cPlusPlus svg"
+        />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/qt/qt-original.svg"
+          alt="Qt svg"
+        />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+          alt="React svg"
+        />
+      </div>
 
-            <div className={styles["tech-stack"]}>
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="HTML5 svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="CSS3 svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="JavaScript svg" />
-            </div>
-        </Card>
-    )
+      <div className={styles["contact-and-project"]}>
+        <Contact />
+        <Projects />
+      </div>
+
+      <div className={styles["tech-stack"]}>
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+          alt="HTML5 svg"
+        />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+          alt="CSS3 svg"
+        />
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+          alt="JavaScript svg"
+        />
+      </div>
+    </Card>
+  );
 }
