63136b0009284571c730df440619f3e07a347cd6
Hermes <hermeschigoziri@gmail.com>
2022-01-29 19:35:21 +0100

    Add ContactItem component to improve code structure

--- src/Components/Main/Contact/Contact.js
+++ src/Components/Main/Contact/Contact.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 import Card from '../../Card/Card';
 import styles from './Contact.module.css';
+
+const ContactItem = (props) => {
+    return (
+        <>
+            <a href={props.link} target="_blank" rel="noreferrer" data-tip data-for={props.iconAlt}>
+                <Card className={styles["contacts-link-card"]}>
+                    <img src={props.iconLink} alt={props.iconAlt} />
+                </Card>
+            </a>
+            <ReactTooltip id={props.iconAlt} type="light">
+                <span>{props.tooltip}</span>
+            </ReactTooltip>
+        </>
+    )
+}
 
 export default function Contact()
 {
@@ -10,29 +26,17 @@ export default function Contact()
             <span>Contact</span>
             
             <div className={styles["contacts"]}>
-                <a href="https://github.com/TruePadawan" target="_blank" rel="noreferrer">
-                    <Card className={styles["contacts-link-card"]}>
-                        <img src="https://img.icons8.com/windows/64/000000/github.png" alt="GitHub PNG"/>
-                    </Card>
-                </a>
+                <ContactItem link="https://github.com/TruePadawan" iconLink="https://img.icons8.com/windows/64/000000/github.png"
+                    iconAlt="GitHub PNG" tooltip="Open a new page to my GitHub" />
 
-                <a href="https://twitter.com/TheTruePadawan" target="_blank" rel="noreferrer">
-                    <Card className={styles["contacts-link-card"]}>
-                        <img src="https://img.icons8.com/color/48/000000/twitter--v1.png" alt="Twitter PNG"/>
-                    </Card>
-                </a>
+                <ContactItem link="https://twitter.com/TheTruePadawan" iconLink="https://img.icons8.com/color/48/000000/twitter--v1.png"
+                    iconAlt="Twitter PNG" tooltip="Open a new page to my Twitter" />
 
-                <a href="mailto:hermeschigoziri@gmail.com" target="_blank" rel="noreferrer">
-                    <Card className={styles["contacts-link-card"]}>
-                        <img src="https://img.icons8.com/fluency/48/000000/mail.png" alt="Mail PNG"/>
-                    </Card>
-                </a>
+                <ContactItem link="mailto:hermeschigoziri@gmail.com" iconLink="https://img.icons8.com/fluency/48/000000/mail.png"
+                    iconAlt="Mail PNG" tooltip="Send me an E-Mail" />
 
-                <a href="https://www.linkedin.com/in/hermes-chigoziri-919148204/" target="_blank" rel="noreferrer">
-                    <Card className={styles["contacts-link-card"]}>
-                        <img src="https://img.icons8.com/color/50/000000/linkedin.png" alt="LinkedIn PNG"/>
-                    </Card>
-                </a>
+                <ContactItem link="https://www.linkedin.com/in/hermes-chigoziri-919148204/" iconLink="https://img.icons8.com/color/50/000000/linkedin.png"
+                    iconAlt="LinkedIn PNG" tooltip="Open a new page to my LinkedIn" />
             </div>
 
         </Card>
